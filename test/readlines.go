4910837dd30d05d960515e83acfda4ba6f494dab
Vivek Nair <vnair611@stanford.edu>
2012-07-29 04:52:48 -0700

    Finished a major part of the IO.

---
+++ test/readlines.go
@@ -0,0 +1,28 @@
+func readLines(path string) (lines []string, err error) {
+    var (
+        file *os.File
+        part []byte
+        prefix bool
+    )
+    if file, err = os.Open(path); err != nil {
+        return
+    }
+    defer file.Close()
+
+    reader := bufio.NewReader(file)
+    buffer := bytes.NewBuffer(make([]byte, 0))
+    for {
+        if part, prefix, err = reader.ReadLine(); err != nil {
+            break
+        }
+        buffer.Write(part)
+        if !prefix {
+            lines = append(lines, buffer.String())
+            buffer.Reset()
+        }
+    }
+    if err == io.EOF {
+        err = nil
+    }
+    return
+}
